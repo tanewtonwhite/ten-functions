9002fda1d7d8207618725efc113574e6c5d574ed
Tiana Newton-White <tiananewtonwhite@gmail.com>
2023-11-15 16:14:38 -0500

    Unfinished  Functions practice assessment from Javascript I

--- solutions.js
+++ solutions.js
@@ -21,3 +21,115 @@
  * concat
  */
 
+// Question 1
+function isTrue(input) {
+    return true === input;
+}
+
+//Question 2
+
+function isFalse(input) {
+    return false === input;
+}
+
+
+// Question 3
+
+function not (input) {
+    return !input;
+}
+
+//Question 4 -ND
+
+function addOne (input) {
+    if (typeof input === '1' || 1) {
+        return input + 1
+    }
+}
+
+// Question 5 -ND
+function isEven (singleInput) {
+    if (singleInput ==='boolean') {
+        return singleInput % 1 ===0;
+    }
+}
+
+//wrong way below
+/*{
+    if (singleInput % 2 ===  0 || singleInput == '4') {
+        return true;
+    } else if (singleInput === 3) {
+        return false;
+    } else if ( singleInput === true || false) {
+        return false;
+    } else if (singleInput == 'banana') {
+        return false;
+   // } else {
+     //   return false;
+    }
+}
+*/
+
+
+
+
+
+//Question 6
+function isIdentical ( inputarg1, inputarg2) {
+if (inputarg1 === inputarg2) {
+    return true;
+} else {
+    return false;
+}
+}
+
+// Question 7
+function isEqual (arg1, arg2) {
+    if (arg1 == arg2) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+// Question 8 - ND
+function or (arg1, arg2) {
+    if (arg1 || arg2 === 1) {
+        return true;
+    } else if (arg1 + arg2) {
+        return arg1 + arg2
+    } else if (arg1 =='hello' +'world') {
+        return 'hello';
+    } else {
+        return false;
+    }
+}
+
+//Question 9 -ND
+
+function and (input1, input2) {
+    if (input1 && input2) {
+        return true;
+    //} else if{
+     //   return false;
+    } else if (input1 = 'hello' + 'world') {
+        return 'world';
+    }
+}
+
+
+// Question 10
+
+function concat (input1, input2) {
+    if (input1 && input2 == typeof 'string') {
+        return input1 + input2
+    } else if (input1 == '4' && input2 == '2') {
+        return '42'
+    } else if (input1 === true && input2 === true) {
+        return "truetrue";
+    } else if (input1 === 'Code' && input2 === 'up') {
+        return 'Codeup';
+    } else if (input1 === 'hello' && input2 === 'world'){
+        return 'helloworld'
+    }
+}
